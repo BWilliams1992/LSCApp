76567bfe90074a21e006723b97f930f136e2d277
Ben Williams <bwilliams0001992@gmail.com>
2020-11-08 14:46:51 +0000

    Update plots controller

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,14 +1,21 @@
 class PlotsController < ApplicationController
-
+  before_action :set_plot, only:[:show]
   def create 
     @plot = Plot.new(plot_params)
     @plot.save
+  end
+
+  def show
   end
 
   private
 
     def plot_params
       params.require(:plot).permit(:number)
+    end
+
+    def set_plot
+      @plot = Plot.find(params[:id])
     end
 
 end
