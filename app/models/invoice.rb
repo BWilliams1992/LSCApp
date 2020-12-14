6ad96055814f3cdab74d4cebd71f358f509ed83f
Ben Williams <bwilliams0001992@gmail.com>
2020-12-14 17:43:06 +0000

    Updated invoice model
    Updated invoice model to have function that will total the cost of the cleans it is linked to and return it as a value.
    Updated locations to show plots that have no houses linked to them

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,22 @@
 class Invoice < ApplicationRecord
   has_many :cleans
   belongs_to :location
+
+    def total_cost 
+      @total = 0
+      self.cleans.each do |clean|
+        @location = self.location
+        @plot = Plot.find(clean.plot_id)
+        @house = House.find(@plot.house_id)
+        @cost_house_location = CostHouseLocation.find_by( location: @location, house: @house )
+        if clean.clean_type == 'Sparkle'
+          @total += @cost_house_location.sparkle_cost
+        elsif clean.clean_type == 'Demo'
+          @total += @cost_house_location.demo_cost
+        elsif clean.clean_type == 'Build'
+          @total += @cost_house_location.build_cost
+        end
+      end
+      return @total
+    end
 end
