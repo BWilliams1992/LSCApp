05eb743c0141af6a8b232298474ae1077eb3d5b4
Ben Williams <bwilliams0001992@gmail.com>
2021-01-29 17:36:43 +0000

    Added function to cleans
    added a function to workout the number of hours spent on a clean . Updated clean show page to have start + end times displayed along with hours worked. Added end +start times to permitted params for clean.

--- app/models/clean.rb
+++ app/models/clean.rb
@@ -1,3 +1,16 @@
 class Clean < ApplicationRecord
   belongs_to :plot
+
+  def hours_worked 
+    if(self.start_time && self.end_time)
+      if (self.start_time < self.end_time)
+        return (self.end_time - self.start_time)/ 3600
+      else
+        return "Start time after end time"
+      end
+    else
+      return "No start or end time"
+    end
+  end
+
 end
