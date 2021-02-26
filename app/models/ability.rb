5b8b1f976b5448db5517c5ee7aa694dcc4687838
Ben Williams <bwilliams0001992@gmail.com>
2021-02-26 17:16:48 +0000

    Fixed typo in ability so staff have proper abilities
    Added staff checkbox to user show page from

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,10 +9,10 @@ class Ability
       can :manage, CleanRequest, user_id: user.id
       if user.staff?
         can :manage, Clean
-        can :view, Location
-        can :view, Invoice
-        can :view, House
-        can :view, Extra
+        can :read, Location
+        can :read, Invoice
+        can :read, House
+        can :read, Extra
       elsif user.admin?
         can :manage, :all
       end
